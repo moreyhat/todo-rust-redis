ce3a0c74c5baba6adbe3355e1adbf3c94362bc0c
moreyhat <moreyhat@gmail.com>
2023-02-12 00:10:44 -0800

    Add React Typescript

---
+++ web/src/components/ToDo.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import ToDo from './ToDo'
+
+test('ToDo is passed as a parameter', () => {
+    const description = "Description for test"
+    render(<ToDo description={description} />)
+    const todoDescription = screen.getByText(description)
+    expect(todoDescription).toBeInTheDocument();
+});
---
+++ web/src/components/ToDo.tsx
@@ -0,0 +1,9 @@
+type ToDoProps = {
+    description: string
+}
+
+const ToDo = (props: ToDoProps) => {
+    return <div>{props.description}</div>
+}
+
+export default ToDo
